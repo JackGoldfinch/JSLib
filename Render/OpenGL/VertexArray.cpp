575b952fa33ccfba8bb3e1160addcdefa7bf3ccc
Johannes Stieglitz <johannes.stieglitz@me.com>
2016-07-28 00:51:43 +0200

    VertexArray is basically finished.

--- Render/OpenGL/VertexArray.cpp
+++ Render/OpenGL/VertexArray.cpp
@@ -12,7 +12,35 @@ namespace JSLib {
 namespace Render {
 namespace OpenGL {
 	
+	VertexArray *VertexArray::_boundVertexArray = nullptr;
 	
+	VertexArray *VertexArray::Bound() {
+		return _boundVertexArray;
+	}
+	
+	bool VertexArray::IsBound ( const VertexArray *vertexArray ) {
+		return _boundVertexArray == vertexArray;
+	}
+	
+	void VertexArray::Bind ( VertexArray *vertexArray ) {
+		if ( _boundVertexArray != vertexArray ) {
+			_boundVertexArray = vertexArray;
+			
+			glBindVertexArray ( _boundVertexArray ? _boundVertexArray->_id : 0 );
+		}
+	}
+	
+	VertexArray::VertexArray() {
+		glGenVertexArrays ( 1, &_id );
+		
+		bind();
+	}
+	
+	VertexArray::~VertexArray() {
+		unbind();
+		
+		glDeleteVertexArrays ( 1, &_id );
+	}
 	
 }
 }
